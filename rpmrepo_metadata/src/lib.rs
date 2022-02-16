856a980b61f4270e952b635035426859515af26f
Daniel Alley <dalley@redhat.com>
2022-02-16 00:20:53 -0500

    Add MPLv2 license notices

--- rpmrepo_metadata/src/lib.rs
+++ rpmrepo_metadata/src/lib.rs
@@ -1,3 +1,9 @@
+// Copyright (c) 2022 Daniel Alley
+// 
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 mod common;
 mod filelist;
 mod metadata;
@@ -9,6 +15,7 @@ mod repository;
 mod updateinfo;
 pub mod utils;
 
+#[cfg(python)]
 mod python;
 
 pub use common::EVR;
